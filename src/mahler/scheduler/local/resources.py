45f081a10fae77c0025d96406e6b06a817a55cc0
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-30 08:29:29 -0500

    Implement LocalResources.{available,submit}

--- src/mahler/scheduler/local/resources.py
+++ src/mahler/scheduler/local/resources.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 :mod:`mahler.scheduler.local.resources -- TODO
-====================================
+==============================================
 
 .. module:: resources
     :platform: Unix
@@ -10,24 +10,36 @@
 TODO: Write long description
 
 """
+import getpass
+import subprocess
+
+import psutil
+
 from mahler.core.resources import Resources
 
 
-class LocalResources(Resources):
-    """
-    """
+CPU_COUNT = psutil.cpu_count()
 
-    def __init__(self):
-        """
-        """
-        pass
+
+class LocalResources(Resources):
+    def __init__(self, processes=CPU_COUNT):
+        self.processes = processes
 
     def available(self):
-        """
-        """
-        raise NotImplemented()
+        out = subprocess.check_output(['ps', '-u', getpass.getuser(), '-o', 'cmd'])
+        n_processes = 0
+        for line in str(out, encoding='utf-8').split("\n"):
+            n_processes += int("mahler execute" in line)
 
-    def submit(self, tasks, **kwargs):
-        """
-        """
-        pass
+        return max(self.processes - n_processes, 0)
+
+    def submit(self, tasks, tags, **kwargs):
+        command = 'mahler execute'
+        if tags:
+            command += " --tags " + " ".join(tags)
+
+        for task in tasks:
+            if self.available():
+                print('Executing command:')
+                print(command)
+                subprocess.Popen(command.split(" "))
