e3a5708da455a7a6de49c89f8ce0275a38a6be36
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-30 08:27:38 -0500

    Add builder for Resources and builder_parser

--- src/mahler/scheduler/local/__init__.py
+++ src/mahler/scheduler/local/__init__.py
@@ -10,6 +10,7 @@
 TODO: Write long description
 """
 from ._version import get_versions
+from .resources import LocalResources, CPU_COUNT
 
 VERSIONS = get_versions()
 del get_versions
@@ -23,6 +24,15 @@ __author_email__ = 'xavier.bouthillier@umontreal.ca'
 __copyright__ = u'2018, Xavier Bouthillier'
 __url__ = 'https://github.com/bouthilx/mahler.scheduler.local'
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+
+def build(processes=CPU_COUNT, **kwargs):
+    return LocalResources(processes=processes)
+
+
+def build_parser(parser):
+    """Return the parser that needs to be used for this command"""
+    local_parser = parser.add_parser('local', help='local help')
+
+    local_parser.add_argument(
+        '--processes', type=int, default=CPU_COUNT,
+        help='number of concurrent process to spawn. Default: number of CPUs available')
